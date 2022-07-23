4e393b9eecac2898815fc9e5c1616badb493a009
iDavidGS <jd_92@live.com.mx>
2022-07-23 00:36:29 -0500

    adding project files

---
+++ README.md
@@ -0,0 +1,18 @@
+# SmartBrain - v2
+Proyecto final del curso ZTM
+
+1. Clonar este repo
+2. Ejecutar `npm install`
+3. Ejecutar `npm start`
+
+** Lee el "Readme" del proyecto Smart-Brain-Api para asegurarte de ejecutar correctamente la app web.
+
+_______________________________________________________________________________________________________
+
+ZTM course final project
+
+1. Clone this repo
+2. Run `npm install`
+3. Run `npm start`
+
+** Read the "Readme" of the Smart-Brain-Api project to make sure you run the web app correctly.
---
+++ src/components/Particles/Particles.js
@@ -0,0 +1,63 @@
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+//import { loadFull } from "tsparticles"; // loads tsparticles
+import { useCallback, useMemo } from "react";
+
+const ParticlesComponent = (props) => {
+    const options = useMemo(() => {
+
+        return {
+            // background: {
+            //     color: "#000", 
+            // },
+            fullScreen: {
+                enable: true,
+                zIndex: -1,
+            },
+            interactivity: {
+                events: {
+                    onClick: {
+                        enable: true,
+                        mode: "push",
+                    },
+                    onHover: {
+                        enable: true,
+                        mode: "repulse",
+                    },
+                },
+                modes: {
+                    push: {
+                        quantity: 5,
+                    },
+                    repulse: {
+                        distance: 100,
+                    },
+                },
+            },
+            particles: {
+                links: {
+                    enable: true,
+                    distance: 200,
+                },
+                move: {
+                    enable: true,
+                    speed: { min: 1, max: 5 },
+                },
+                opacity: {
+                    value: { min: 0.3, max: 0.7 },
+                },
+                size: {
+                    value: { min: 1, max: 3 },
+                },
+            },
+        };
+    }, []);
+
+    const particlesInit = useCallback((engine) => {
+        loadSlim(engine);
+    }, []);
+
+    return <Particles id={props.id} init={particlesInit} options={options} />;
+};
+
+export default ParticlesComponent;
